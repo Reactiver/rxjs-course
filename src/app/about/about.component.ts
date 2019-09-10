77c04c071498118e9e359a1ad2d1c84e20e54313
Dima <purisdima@mail.ru>
2019-09-10 23:59:01 +0300

    add courses

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
-
 }
