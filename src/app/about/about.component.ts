d8f2423c157d0028699ff8048abb0b74a16b087b
Dima Puris <purisdima@gmail.com>
2019-09-12 11:52:19 +0300

    15 lesson complete

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { of, concat, interval } from 'rxjs';
+import { debounce, debounceTime, take } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -9,5 +11,11 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
+    const source1$ = interval(1000).pipe(take(5));
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
+    const result$ = concat(source2$, source1$, source3$)
+
+    result$.subscribe(console.log)
   }
 }
