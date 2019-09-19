5c02d52d4bc5551b2503f92ed6a317bda7ed9c81
Dima Puris <purisdima@gmail.com>
2019-09-19 11:54:46 +0300

    86 percent complete

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject, AsyncSubject, ReplaySubject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -13,7 +13,19 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
+        const subject = new ReplaySubject();
 
+        const series$ = subject.asObservable();
+        series$.subscribe(value => console.log('early sub:' + value))
+
+        subject.next(1);
+        subject.next(2);
+        subject.next(3);
+        // subject.complete();
+        setTimeout(() => {
+            series$.subscribe(value => console.log('late sub:' + value))
+        }, 3000)
+        
     }
 
 
---
+++ src/app/common/store.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from "@angular/core";
+import { Observable, Subject, BehaviorSubject, timer } from "rxjs";
+import { Course } from "../model/course";
+import { createHttpObservable } from "./util";
+import { tap, map, shareReplay, retryWhen, delayWhen } from "rxjs/operators";
+import { fromPromise } from "rxjs/internal-compatibility";
+
+@Injectable({
+  providedIn: "root"
+})
+export class Store {
+  private subject = new BehaviorSubject<Course[]>([]);
+
+  courses$: Observable<Course[]> = this.subject.asObservable();
+
+  init() {
+    const http$ = createHttpObservable("/api/courses");
+
+    http$
+      .pipe(
+        tap(() => console.log("HTTP request executed")),
+        map(res => Object.values(res["payload"]))
+      )
+      .subscribe(courses => this.subject.next(courses));
+  }
+
+  selectBeginnerCourses(): Observable<Course[]> {
+    return this.filterByCategory("BEGINNER");
+  }
+
+  selectAdvancedCourses(): Observable<Course[]> {
+    return this.filterByCategory("ADVANCED");
+  }
+
+  filterByCategory(category: string) {
+    return this.courses$.pipe(
+      map(courses => courses.filter(course => course.category == "BEGINNER"))
+    );
+  }
+
+  saveCourse(courseId: number, changes) {
+    const courses = this.subject.getValue();
+    const courseIndex = courses.findIndex(course => course.id == courseId);
+    const newCourses = courses.slice(0);
+    newCourses[courseIndex] = {
+      ...courses[courseIndex],
+      ...changes
+    };
+
+    this.subject.next(newCourses);
+
+    return fromPromise(fetch(`/api/courses/${courseId}`, {
+      method: 'PUT',
+      body: JSON.stringify(changes),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }));
+  }
+}
